8bc5a364b255715c4cd2ac87e97be9741a964685
aespinosav <alonso.espinosa@gmail.com>
2014-07-08 15:49:11 +0100

    I rewrote most of the functions 'properly' i think. Th data is now mostly stored in lists and numpy arrays. Dicts are used for counting the words (making the distributions)

--- entropy_calculation.py
+++ entropy_calculation.py
@@ -4,15 +4,18 @@ from parsing import *
 
 spike_times = np.loadtxt("Sfly01508SpikeTimes.txt")
 
-repeated_exp = split_into_runs(spike_times, 5, 1000)
+repeated_exp = split_into_runs(spike_times, 200, 1000)
 
-experiments_letters = [make_letters(i, int(i[0]), 5, 0.003) for i in repeated_exp]
+experiments_letters = [convert_to_letters(i, 0.003, int(i[0]), 5) for i in repeated_exp]
 
-experiments_words = [split_into_words(d, 10) for d in experiments_letters]
+experiments_words = [convert_to_words(d, 10) for d in experiments_letters]
 
 experiment_distributions = [word_distribution(e) for e in experiments_words]
 
-time_distributions = word_distribution_given_time(experiments_words)
+time_distributions = word_distributions_given_t(experiments_words)
+
+
+
 
 entropy_total = calculate_entropy(experiment_distributions[0])
 
--- parsing.py
+++ parsing.py
@@ -3,7 +3,22 @@ import numpy as np
 #split data into individual runs (5 sec chunks)
 #experiment_runs = [[] for i in range(20)]
 
-def split_into_runs(spike_data, length_of_run, total_length):
+#def split_into_runs(spike_data, length_of_run, total_length):
+    #"""
+    #Splits the spiking data array into arrays for each run.
+
+    #spike_data - a numpy array
+    #length_of_run - time of individual experiment (in seconds)
+    #total_length - time of total data sample (in seconds)
+    #Returns an array of arrays (numpy).
+    #"""
+
+    #experiment_runs = [spike_data[(start <= spike_data) & (spike_data <= start + length_of_run)] - start
+                       #for start in range(0, total_length + length_of_run, length_of_run) ]
+
+    #return np.array(experiment_runs)
+
+def split_into_runs(spike_data, number_of_runs, total_length):
     """
     Splits the spiking data array into arrays for each run.
 
@@ -12,97 +27,200 @@ def split_into_runs(spike_data, length_of_run, total_length):
     total_length - time of total data sample (in seconds)
     Returns an array of arrays (numpy).
     """
+    length_of_run = total_length/float(number_of_runs)
+    array_of_runs = []
 
-    experiment_runs = [spike_data[(start <= spike_data) & (spike_data <= start + length_of_run)] - start
-                       for start in range(0, total_length + length_of_run, length_of_run) ]
+    for i in range(number_of_runs):
 
-    return np.array(experiment_runs)
+        start = i*length_of_run
+        stop = start + length_of_run
+
+        run_data = spike_data[(start <= spike_data)&(spike_data <= stop)]
+
+        array_of_runs.append(run_data)
+
+    array_of_runs = [x for x in array_of_runs if len(x) != 0]
+
+    print "{} runs were found".format(len(array_of_runs))
+
+
+    return np.array(array_of_runs)
 
 
 
-def make_letters(spike_times, start_time,  length_of_data, letter_interval=0.003):
+#def make_letters(spike_times, start_time,  length_of_data, letter_interval=0.003):
+    #"""
+    #Parses spike time data into letters.
+
+    #Each letter is made by temporally binning spiking data (either 1 or 0)
+    #letter_interval - the size of the bins (in seconds)
+    #length_of_data - the length of the experiment (in seconds)
+
+    #Returns a dictionary  {bin number : state}
+    #"""
+
+
+    #number_of_bins = int(length_of_data/letter_interval)
+
+    #bins = {i:0 for i in range(number_of_bins)}
+
+    #data = set(map(int, (spike_times - start_time)/letter_interval))
+    #data = list(data)
+
+    #for i in data:
+        #bins[i] = 1
+
+    #return bins
+
+def convert_to_letters(spike_data, letter_interval, start_time, experiment_time):
     """
     Parses spike time data into letters.
 
     Each letter is made by temporally binning spiking data (either 1 or 0)
     letter_interval - the size of the bins (in seconds)
-    length_of_data - the length of the experiment (in seconds)
+    start__time = start time (in seconds) of the experiment
+    experiment_time - the length of the experiment (in seconds)
+    Returns a lsit of letter numpy arrays
 
-    Returns a dictionary  {bin number : state}
+    only bins that are fully contained within the experiment time are considered
     """
+    number_of_letters = int(experiment_time / letter_interval)
+    letter_array = np.zeros(number_of_letters).astype(int)
+
+    spike_bins = (spike_data - start_time)/letter_interval
+    spike_bins = map(int, spike_bins)
+
+    for i in spike_bins:
+        letter_array[i] = 1
+
+    return letter_array
 
 
-    number_of_bins = int(length_of_data/letter_interval)
-
-    bins = {i:0 for i in range(number_of_bins)}
-
-    data = set(map(int, (spike_times - start_time)/letter_interval))
-    data = list(data)
-
-    for i in data:
-        bins[i] = 1
-
-    return bins
 
 
-def split_into_words(data_dictionary, word_length):
+#def split_into_words(data_dictionary, word_length):
+    #"""
+    #Splits data that has been converted into letters into words
+    #returns dictionary with words
+
+    #data_dictionary - dict of bin:letter
+    #word_length - number of letters in a word
+    #"""
+
+    #word_dict = {}
+    #current_key = 0
+    #current_word = ""
+    
+    #for i in data_dictionary.iterkeys():
+        
+        #if i>0 and i%word_length == 0:
+            
+            #word_dict[current_key] = current_word
+            ##print current_word
+            
+            #current_key += 1
+            #current_word = ""
+
+
+        #current_word += str(data_dictionary[i])
+
+    #return word_dict
+
+def convert_to_words(letter_exp, word_length):
     """
     Splits data that has been converted into letters into words
-    returns dictionary with words
+    returns a list of words
 
-    data_dictionary - dict of bin:letter
+    letter_exp - an array or list of letters for an experiment
     word_length - number of letters in a word
     """
 
-    word_dict = {}
-    current_key = 0
-    current_word = ""
-    
-    for i in data_dictionary.iterkeys():
-        
-        if i>0 and i%word_length == 0:
-            
-            word_dict[current_key] = current_word
-            #print current_word
-            
-            current_key += 1
-            current_word = ""
+    number_of_words = int(len(letter_exp) / float(word_length))
+
+    word_list = []
+
+    for i in range(number_of_words):
+        current_word = ''
+        for j in range(word_length):
+            current_word += str(letter_exp[i*word_length + j])
+
+        word_list.append(current_word)
+
+    return word_list
 
 
-        current_word += str(data_dictionary[i])
-
-    return word_dict
     
 
-def word_distribution(word_dict):
+def word_distribution(words):
     """
-    Counts words. Returns dict that has the word (string) as a key
+    Counts words and returns and rturns dict that has the word (string) as a key
     and the valued is the frecuency.
 
-    word_dict - dictionary of words (like the one that is returned by the split_into_words function)
+    words - dictionary of words (like the one that is returned by the split_into_words function)
     """
 
     distribution_dict = {}
-    for i in word_dict.itervalues():
-        if distribution_dict.has_key(i):
-            distribution_dict[i] += 1
+    for w in words:
+        if distribution_dict.has_key(w):
+            distribution_dict[w] += 1
         else:
-            distribution_dict[i] = 1
+            distribution_dict[w] = 1
 
     return distribution_dict
 
     
-def word_distribution_given_time(array_of_experiments):
+#def word_distribution_given_time(array_of_experiments):
+    #"""
+    #Compiles a distribution dic for words given the time at which they happened (word for word)
+
+    #array_of_experiments - an array of the data converted into words.
+    #returns a distribution dictionary like the one returned by word_distribution.
+    #"""
+
+    #distribution_array = []
+
+    #word_lists = [[d[i] for d in array_of_experiments] for i in array_of_experiments[0].iterkeys()]
+
+    #for l in word_lists:
+        #timestep_dict = {}
+
+        #for j in l:
+            #if timestep_dict.has_key(j):
+                #timestep_dict[j] += 1
+            #else:
+                #timestep_dict[j] = 1
+
+        #distribution_array.append(timestep_dict)
+    
+    ##for i in array_of_experiments[0].iterkeys():
+        
+        ##timestep_dict = {}
+        ##word_list = [d[i] for d in array_of_experiments]
+        
+        ##for j in word_list:
+            ##if timestep_dict.has_key(j):
+                ##timestep_dict[j] += 1
+            ##else:
+                ##timestep_dict[j] = 1
+                
+        ##distribution_array.append(timestep_dict)
+
+    #return distribution_array
+
+
+def word_distributions_given_t(array_of_experiments):
     """
-    Compiles a distribution dic for words given the time at which they happened (word for word)
+    Compiles a distribution dic for words given the time at which they happened.
 
     array_of_experiments - an array of the data converted into words.
     returns a distribution dictionary like the one returned by word_distribution.
-    """
 
+    The experiments must have the same number of words!
+    """
     distribution_array = []
 
-    word_lists = [[d[i] for d in array_of_experiments] for i in array_of_experiments[0].iterkeys()]
+    word_lists = [[d[i] for d in array_of_experiments]
+                   for i in range(len(array_of_experiments[0]))]
 
     for l in word_lists:
         timestep_dict = {}
@@ -114,22 +232,8 @@ def word_distribution_given_time(array_of_experiments):
                 timestep_dict[j] = 1
 
         distribution_array.append(timestep_dict)
-    
-    #for i in array_of_experiments[0].iterkeys():
-        
-        #timestep_dict = {}
-        #word_list = [d[i] for d in array_of_experiments]
-        
-        #for j in word_list:
-            #if timestep_dict.has_key(j):
-                #timestep_dict[j] += 1
-            #else:
-                #timestep_dict[j] = 1
-                
-        #distribution_array.append(timestep_dict)
 
     return distribution_array
-
     
 
 def number_of_words_observed(distribution_dict):
@@ -147,7 +251,7 @@ def calculate_entropy(distribution_dict):
     distribution_dict - Dictionary with keys for words / freq for values
     """
 
-    total_words = number_of_words_observed(distribution_dict)
+    total_words = sum(distribution_dict.values())
     P = [float(i)/total_words for i in distribution_dict.itervalues()]
     
     entropy = -sum(P*np.log2(P))
