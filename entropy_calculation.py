1530d103888618da323496859445b1a745afa4c7
aespinosav <alonso.espinosa@gmail.com>
2014-07-29 17:22:20 +0100

    Just messed around a bit, nothing changed

--- entropy_calculation.py
+++ entropy_calculation.py
@@ -1,7 +1,6 @@
 #import numpy as np
 from parsing import *
 
-
 spike_times = np.loadtxt("Sfly01508SpikeTimes.txt")
 
 repeated_exp = split_into_runs(spike_times, 200, 1000)
@@ -13,9 +12,6 @@ experiments_words = [convert_to_words(d, 10) for d in experiments_letters]
 experiment_distributions = [word_distribution(e) for e in experiments_words]
 
 time_distributions = word_distributions_given_t(experiments_words)
-
-
-
 
 entropy_total = calculate_entropy(experiment_distributions[0])
 
