bd1a569700e1fe53520576ebd60d26bbb4966c5d
aespinosav <alonso.espinosa@gmail.com>
2014-08-01 19:20:26 +0100

    Added 2 new n

--- kernel_metric.py
+++ kernel_metric.py
@@ -17,7 +17,7 @@ import scipy as sp
     
     #return vals
     
-def boxcar(t, dt):
+def boxcar(t, bandwidth):
     
     N = len(t)
     vals = np.zeros(N)
@@ -29,3 +29,29 @@ def boxcar(t, dt):
     return vals
     
 #def convolve(spike_letters, kernel)
+
+def boxcar2(y, t, i, bandwidth):
+    N = len(y)
+    vals = np.zeros(N)
+    
+    for j in range(N):
+        if t[i] > -dt/2.0 and t[i] < dt/2.0:
+            vals[i] = 1.0/dt
+
+
+def kernelise(self, kernel):
+    """
+    Returns an array where the kernels have been added for 
+    the spiking times.
+    
+    kernel - kernel function that takes an array, a value and a bandwidth as arguments
+    """
+    
+    
+    arglist = np.argwhere(self.hist[0]) #arg list of nonzerom times
+    
+    y = np.zeros(len(self.t))
+    for i in arglist:
+        y += kernel(y, t, i, bandwidth)
+        
+    self.kern_function = y
