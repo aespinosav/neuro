e7ef9270f34a9834b74511cd373bc7891e6afdfb
aespinosav <alonso.espinosa@gmail.com>
2014-09-08 12:55:54 +0100

    Added scripts for plotting results from KDE method

---
+++ plot_mi_vs_ns.py
@@ -0,0 +1,46 @@
+"""
+This file takes data from calculation
+to see the sensitivity of the kde method
+of calculating the mutual information and
+plots the corresponding graph.
+
+MI as the number of trials stimuli
+different lines are used for different
+number of trials.
+"""
+
+from spike_train_object import *
+from parsing import *
+from sys import argv
+
+import matplotlib.pyplot as plt
+
+data_file_name = argv[1]
+
+data = np.loadtxt(data_file_name)
+
+#The columns of the data correspond to 
+##ns nt  max_information information_conor information_alonso
+
+#First we sort the data by nt
+
+data = data[data[:,1].argsort()]
+
+nt_list = list(set([n for n in data[:,1]]))
+nt_list.sort()
+
+data_for_lines = np.array([[d for d in data if d[1]==nt] for nt in nt_list])
+data_for_lines = np.array([ d[d[:,0].argsort()] for d in data_for_lines])
+
+for nt in nt_list:
+
+    indx = nt_list.index(nt)
+    label="$n_t =$ {}".format(int(nt))
+    INFO = data_for_lines[indx,:,3]/data_for_lines[indx,:,2]
+    NS = data_for_lines[indx,:,0]
+        
+    plt.plot(NS, INFO, label=label)
+    
+#plt.legend(loc='lower left', fancybox=True)
+plt.xlabel("$n_s$")
+plt.ylabel("Mutual Information/ Max Information")
---
+++ plot_mi_vs_nt.py
@@ -0,0 +1,45 @@
+"""
+This file takes data from calculation
+to see the sensitivity of the kde method
+of calculating the mutual information and
+plots the corresponding graph.
+
+MI as the number of trials increases
+different lines are used for different
+number of stimuli.
+"""
+
+from spike_train_object import *
+from parsing import *
+from sys import argv
+
+import matplotlib.pyplot as plt
+
+data_file_name = argv[1]
+
+data = np.loadtxt(data_file_name)
+
+#The columns of the data correspond to 
+##ns nt  max_information information_conor information_alonso
+
+#First we sort the data by nt
+
+data = data[data[:,1].argsort()]
+
+ns_list = list(set([n for n in data[:,0]]))
+ns_list.sort()
+
+data_for_lines = np.array([[d for d in data if d[0]==ns] for ns in ns_list])
+
+for ns in ns_list[-5:]:
+
+    indx = ns_list.index(ns)
+    label="$n_s =$ {}".format(int(ns))
+    INFO = data_for_lines[indx,:,3]
+    NT = data_for_lines[indx,:,1]
+        
+    plt.plot(NT, INFO, label=label)
+    
+plt.legend(loc='lower left', fancybox=True)
+plt.xlabel("$n_t$")
+plt.ylabel("Mutual Information [bits]")
