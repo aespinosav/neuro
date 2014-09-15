90c843aa16fe1d035eeacdc5ba0235f9fa371fb7
aespinosav <alonso.espinosa@gmail.com>
2014-09-15 14:27:41 +0100

    worked on clustering.jl initialising seeds only works when there are no zero spike trains, otherwise it fails. Also, medoids lose their clusters

--- clustering.jl
+++ clustering.jl
@@ -3,12 +3,15 @@ using Clustering
 directory = ARGS[1]
 files = readdir(directory)
 
-function assigned_class(i, ns, km)
+function assigned_class(i, km)
+    ns = length(km.medoids)
+
     cluster = km.assignments[i]
     cluster_stim = km.medoids[cluster]%ns + 1
 end
 
-function real_class(i, ns)
+function real_class(i, km)
+    ns = length(km.medoids)
     stim = i%ns + 1
 end
 
@@ -19,14 +22,44 @@ function conf_matrix(km)
     matrix = zeros(Int, ns, ns)
     
         for i in range(1,length(km.assignments))
-            rc = real_class(i, ns)
-            ac = assigned_class(i, ns, km)
+            rc = real_class(i, km)
+            ac = assigned_class(i, km)
             
             matrix[rc, ac] += 1
         end
         
     return matrix
 end
+
+#
+#function counts_good_index(km)
+#    
+#    ns = length(km.medoids)
+#    
+#    counts_good = zeros(ns)
+    
+#    for i in range(1, ns)
+#        real_c = real_class(km.medoids[i], km)
+#        counts = km.counts[i]
+#        
+#        counts_good[real_c] = counts
+#    end
+#    
+#    return counts_good
+#end
+
+function counts_good_index(confusion_matrix, ns)
+    
+    #ns = length(conf_matrix[1,:])
+    counts_good = Int[]
+    
+    for i in range(1, ns)
+        push!(counts_good, sum(confusion_matrix[:,i]))
+    end
+    
+    return counts_good
+end
+
 
 for f in files
 
@@ -51,12 +84,14 @@ for f in files
     confusion_matrix = conf_matrix(km)
     
     
-    
     str1 = "ns = $ns; nt = $nt; nr = $nr\nClusters match: $(length(km.counts)==ns)"
     println(str1)
-    println(km.counts, "\n")
+    println("Counts:\n$(counts_good_index(confusion_matrix, ns)) \n")
     println("Confusion Matrix:\n", confusion_matrix, "\n" )
-    #println(km.assignments)
+    
+    for k in km.medoids
+        println(real_class(k, km))
+    end
     
 
     
