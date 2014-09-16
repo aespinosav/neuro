a22342a91544060ba62abf63b984a9044e7f2d3f
aespinosav <alonso.espinosa@gmail.com>
2014-09-16 20:40:01 +0100

    script to plot comparison of mutual information with kde and discrete method

---
+++ plot_comparison_disc_kde.py
@@ -0,0 +1,15 @@
+data = np.loadtxt("data_meeting_conor.dat") #kde data
+data_discrete = np.loadtxt("discrete_data_matching_meeting.dat")
+data_discrete = data_discrete[data_discrete[:,0].argsort()]
+
+plt.plot(data[:,2], data[:,-1], "o-k", label="KDE method")
+plt.plot(data_discrete[:,0], data_discrete[:,-1], "o-g", label="Discrete method")
+
+plt.hlines(np.log2(166), 10, 70, linestyle="--")
+
+plt.grid(True)
+plt.ylim([4, 7.5])
+plt.legend(loc='lower left' )
+
+plt.xlabel("$n_t$", fontsize=18)
+plt.ylabel("$I(R,S)$ [bits]", fontsize=18)
