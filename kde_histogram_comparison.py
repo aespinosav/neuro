30d87ce0fd1e8a8e7ee931ed5315f4e99bc49b0a
aespinosav <alonso.espinosa@gmail.com>
2014-09-17 18:58:51 +0100

    final version of the script used to plot the grphs needed.

--- kde_histogram_comparison.py
+++ kde_histogram_comparison.py
@@ -45,8 +45,49 @@ ax[1].set_yticks(np.arange(0,max_tick+0.05, 0.05))
 
 #fig.ylabel("Probability density")
 #ax[1].ylabel("Probability density")
-ax[0].set_ylabel("Probability density")
-ax[1].set_ylabel("Probability density")
+ax[0].set_ylabel("Probability density", fontsize=14)
+ax[1].set_ylabel("Probability density", fontsize=14)
 fig.tight_layout()
 
 plt.xlim([-5,15])
+
+
+#######################################################
+KDE estimation
+#######################################################
+
+
+
+
+
+
+N = 100
+
+X = np.concatenate((np.random.normal(-1, 1, 0.1*N),
+                    np.random.normal(3, 1, 0.2*N),
+                    np.random.normal(7, 2, 0.7*N)))[:, np.newaxis]
+                    
+X_plot = np.linspace(-5, 15, 1000)[:,None]
+
+
+
+fig2, ax2 = plt.subplots()
+
+kernel = 'gaussian'
+
+
+ax2.fill(X_plot[:,0], true_density, fc='black', alpha=0.3, label='True distribution')
+
+bw_range = [0.3, 0.5, 1.5]
+
+for bandwidth in  bw_range:
+    kde = KernelDensity(kernel=kernel, bandwidth=bandwidth).fit(X)
+    log_dens = kde.score_samples(X_plot)
+    ax2.plot(X_plot[:,0], np.exp(log_dens), '-', label='Bandwidth = {}'.format(bandwidth))
+
+ax2.plot(X[:,0], np.zeros(len(X)), "or", markersize=5)
+    
+ax2.legend(loc='upper left')
+ax2.set_ylabel('Probability density')
+ax2.text(6, 0.38, "N={0} points".format(N))
+fig2.tight_layout()
